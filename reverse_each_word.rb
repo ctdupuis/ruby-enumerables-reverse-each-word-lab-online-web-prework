5e0f0c68f3609c4c2534693e84b9055d9b713bcc
Cody Dupuis <ctdupuis7@gmail.com>
2019-08-29 02:09:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,7 @@
 def reverse_each_word(string)
  splarray = string.split
  i = 0
- newspl = []
- 
-  while i < splarray.length do
-    newspl.collect { |elm| elm[i].reverse }
-    i += 1
+ newspl = splarray.collect { |array| array.reverse }
   end
   newspl
   #revstring = newspl.join(" ")
